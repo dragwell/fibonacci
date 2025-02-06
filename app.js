6641be1627f3ec58452cfbec6a6d319b883f8c62
dragwell <197752333+dragwell@users.noreply.github.com>
2025-02-06 18:19:29 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
   if(typeof n !== 'number')return null;
-  switch (n) {
-    case 0 : return 0;
-    case 1 : return 1;
-    default : return fib(n - 1) + fib(n - 2);
-  }
+  if(memo.has(n))return memo.get(n);
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
 }
 const len = 40;
 for(let i = 0; i <= len; i++)console.log(fib(i));
