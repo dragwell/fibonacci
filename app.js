93884455c237b1ae8169e86edbcb26bd152b2f05
dragwell <197752333+dragwell@users.noreply.github.com>
2025-02-06 18:11:24 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+function fib(n) {
+  if(typeof n !== 'number')return null;
+  switch (n) {
+    case 0 : return 0;
+    case 1 : return 1;
+    default : return fib(n - 1) + fib(n - 2);
+  }
+}
+const len = 40;
+for(let i = 0; i <= len; i++)console.log(fib(i));
